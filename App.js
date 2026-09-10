019101c2dd8e1a1c9e0801f4078daa42b1c6635c
IsraelRufus-RJ <israelrufus2000@gmail.com>
2026-09-10 11:40:49 +0530

    Add Start Script and Functional Component using JSX

--- App.js
+++ App.js
@@ -1,37 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const jsxHeading = <div>
+    <h3 id="heading">Sri Lanka</h3>
+</div>
 
-const heading = React.createElement("h1", {id:"heading", xyz: "abc"}, "Hello World from react!!!");
-console.log("React Element", heading);
+const CardComponent = () => {
+    return <div className="card">
+        <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2026/6/19/76333369-ea7c-4dd8-990e-a3c22defc234_1374812.jpg"/>
+        <h2>{jsxHeading}</h2>
+        <p>Despite its fame as a resplendent island and a leading tourist destination, the secret of Sri Lanka’s attraction lies with its people. The spice addicted, cricket crazy and tea drinking people of Sri Lanka are famed for big smiles and a bigger heart. With a culture enriched with three thousand years of knowledge, Buddhism and many a colonial traditions, the Sri Lankan society is a potpourri of religions and races different yet similar in many a ways.</p>
+    </div>
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log("Root",  root);
-
-// <div id="parent">
-//     <div id="child1">
-//         <h1>I'm H1 tag</h1>
-//         <h2>I'm H2 tag</h2>
-//     </div>
-//     <div id="child2">
-//         <h1>I'm H1 tag</h1>
-//         <h2>I'm H2 tag</h2>
-//     </div>
-// </div>
-
-const parent = React.createElement("div", {id: "parent"}, 
-    [React.createElement("div", {id: "child1"}, 
-        [
-            React.createElement("h1", {}, "I'm H1 Tag."),
-            React.createElement("h2", {}, "I'm H2 Tag")
-        ]
-    ),
-    React.createElement("div", {id: "child2"}, 
-        [
-            React.createElement("h1", {}, "I'm H1 Tag."),
-            React.createElement("h2", {}, "I'm H2 Tag")
-        ]
-    )]
-);
-
-root.render(parent);
+root.render(<div className="cards"><CardComponent/><CardComponent/><CardComponent/><CardComponent/><CardComponent/><CardComponent/><CardComponent/></div>);
