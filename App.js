42f578606a59287b200e897437b1db2b35927632
IsraelRufus-RJ <israelrufus2000@gmail.com>
2026-09-21 17:33:04 +0530

    Added Mock Data and Restaurant Card, and popular dish List

--- App.js
+++ App.js
@@ -1,17 +1,322 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import foodCarousalImg from "./data"
 
-const jsxHeading = <div>
-    <h3 id="heading">Sri Lanka</h3>
-</div>
+const restaurantList = [
+  {
+    "id": "726795",
+    "name": "Madras Coffee House",
+    "imageId": "aec5b21b11aa335a59d724f01db1bb32",
+    "cuisines": [
+      "Beverages",
+      "Snacks"
+    ],
+    "rating": 4.4,
+    "location": "New Concourse Area, Egmore",
+    "estimatedTimeArrival": "25-30 mins"
+  },
+  {
+    "id": "789332",
+    "name": "Bask by Coffee",
+    "imageId": "5b17fc2fe703cfaa5478f2994fdf2e1d",
+    "cuisines": [
+      "Beverages",
+      "Italian",
+      "Snacks",
+      "Thai"
+    ],
+    "rating": 4.5,
+    "location": "Teynampet, Teynampet",
+    "estimatedTimeArrival": "55-65 mins"
+  },
+  {
+    "id": "708880",
+    "name": "KFC",
+    "imageId": "RX_THUMBNAIL/IMAGES/VENDOR/2026/7/1/7e87b8ba-8317-4daa-8035-0742b94a1fd5_708880.JPG",
+    "cuisines": [
+      "Burgers",
+      "Fast Food",
+      "Rolls & Wraps"
+    ],
+    "rating": 4.2,
+    "location": "Sydenhams Road, Periyampet",
+    "estimatedTimeArrival": "20-25 mins"
+  },
+  {
+    "id": "35323",
+    "name": "Udipi Hotel Sudha",
+    "imageId": "RX_THUMBNAIL/IMAGES/VENDOR/2026/8/25/0bc95cb8-b76a-4095-acf5-92886abf5b26_35323.jpg",
+    "cuisines": [
+      "South Indian",
+      "Chinese",
+      "North Indian",
+      "Jain",
+      "Snacks",
+      "Chaat",
+      "sandwich",
+      "Biryani",
+      "Desserts",
+      "Continental"
+    ],
+    "rating": 4.4,
+    "location": "Egmore, Egmore",
+    "estimatedTimeArrival": "30-40 mins"
+  },
+  {
+    "id": "24273",
+    "name": "Hotel Shiva Sagar",
+    "imageId": "srgkvzgrhtk7lyotwk0a",
+    "cuisines": [
+      "South Indian",
+      "Jain",
+      "Chaat",
+      "Snacks",
+      "Chinese",
+      "Beverages",
+      "Juices",
+      "Desserts"
+    ],
+    "rating": 4.4,
+    "location": "Vepery, Vepery",
+    "estimatedTimeArrival": "25-30 mins"
+  },
+  {
+    "id": "726146",
+    "name": "Theobroma",
+    "imageId": "RX_THUMBNAIL/IMAGES/VENDOR/2026/9/2/733e4066-2e09-4605-9443-2787f8540a69_726146.JPG",
+    "cuisines": [
+      "Desserts"
+    ],
+    "rating": 4.6,
+    "location": "Purasaiwakkam, Purasaiwakkam",
+    "estimatedTimeArrival": "30-35 mins"
+  },
+  {
+    "id": "954732",
+    "name": "Govindam",
+    "imageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/9/13/24a413dc-5ee9-402e-b3a1-fc038eac61af_954732 (1).jpg",
+    "cuisines": [
+      "Desserts",
+      "Rajasthani",
+      "Beverages",
+      "Sweets",
+      "Healthy Food",
+      "Cafe"
+    ],
+    "rating": 4.7,
+    "location": "Kilpauk, Kilpauk",
+    "estimatedTimeArrival": "25-30 mins"
+  },
+  {
+    "id": "641587",
+    "name": "The Cane Station",
+    "imageId": "jrp91rzvnysg9bv5il7s",
+    "cuisines": [
+      "Beverages"
+    ],
+    "rating": 4.6,
+    "location": "Ayanavaram, Perambur",
+    "estimatedTimeArrival": "45-50 mins"
+  },
+  {
+    "id": "38622",
+    "name": "Parvathi Bavan Sweets",
+    "imageId": "bn7xohd11lzytfeeudyc",
+    "cuisines": [
+      "Sweets",
+      "Snacks",
+      "Chaat",
+      "Desserts"
+    ],
+    "rating": 4.4,
+    "location": "Alwarpet, Teynampet",
+    "estimatedTimeArrival": "40-45 mins"
+  },
+  {
+    "id": "1392115",
+    "name": "Lighthouse Coffee",
+    "imageId": "RX_THUMBNAIL/IMAGES/VENDOR/2026/6/19/e9cfea18-7603-4300-81f1-83b1f7cdd36b_1392115.jpg",
+    "cuisines": [
+      "Beverages",
+      "Burgers",
+      "Cafe",
+      "Indian",
+      "Italian",
+      "Fast Food"
+    ],
+    "rating": 4.2,
+    "location": "Mylapore, Mylapore",
+    "estimatedTimeArrival": "40-45 mins"
+  },
+  {
+    "id": "611416",
+    "name": "Everest Bakers & Cafe",
+    "imageId": "8d99abf1ced61a747253f050ff4f70a0",
+    "cuisines": [
+      "Snacks",
+      "Bakery"
+    ],
+    "rating": 4.5,
+    "location": "Mh Road, Perambur",
+    "estimatedTimeArrival": "45-50 mins"
+  },
+  {
+    "id": "10834",
+    "name": "Basil With A Twist",
+    "imageId": "j6xodxgd7hukpzkdzzz4",
+    "cuisines": [
+      "Italian",
+      "European"
+    ],
+    "rating": 4.5,
+    "location": "Thyagaraya Nagar, Thyagaraya Nagar",
+    "estimatedTimeArrival": "45-50 mins"
+  },
+  {
+    "id": "112136",
+    "name": "Hotel Parijatha - G.A Road",
+    "imageId": "t2mzebvn1rvgnqtrucrq",
+    "cuisines": [
+      "South Indian",
+      "Chinese",
+      "Biryani"
+    ],
+    "rating": 4.5,
+    "location": "Washermanpet, Washermenpet",
+    "estimatedTimeArrival": "45-50 mins"
+  },
+  {
+    "id": "1320467",
+    "name": "The Krishna Cafe",
+    "imageId": "RX_THUMBNAIL/IMAGES/VENDOR/2026/1/30/08c868cf-ae23-4f1d-8b0e-8a5114b462c2_1320467.jpg",
+    "cuisines": [
+      "South Indian"
+    ],
+    "rating": 4.4,
+    "location": "Mount Road, Royapettah",
+    "estimatedTimeArrival": "40-45 mins"
+  },
+  {
+    "id": "404428",
+    "name": "Biriyani Mubaarak",
+    "imageId": "RX_THUMBNAIL/IMAGES/VENDOR/2026/7/7/67106a5f-6149-4bd3-af76-1013d8b23f69_404428.JPG",
+    "cuisines": [
+      "Biryani"
+    ],
+    "rating": 4.2,
+    "location": "Aminjikarai, Aminjikarai",
+    "estimatedTimeArrival": "40-45 mins"
+  },
+  {
+    "id": "686179",
+    "name": "Aruvi Veg Restaurant",
+    "imageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/5/21/e439a253-eb04-453d-9e8d-f77923a15ed9_686179.ss.jpg",
+    "cuisines": [
+      "South Indian",
+      "Chinese",
+      "North Indian",
+      "Biryani",
+      "Jain"
+    ],
+    "rating": 4.3,
+    "location": "Thyagaraya Nagar, T Nagar",
+    "estimatedTimeArrival": "50-60 mins"
+  },
+  {
+    "id": "783020",
+    "name": "Nithya Amirtham",
+    "imageId": "RX_THUMBNAIL/IMAGES/VENDOR/2026/9/11/139ea918-06b4-424d-8496-97abeabf0932_783020.jpg",
+    "cuisines": [
+      "South Indian",
+      "Indian",
+      "Snacks",
+      "Beverages"
+    ],
+    "rating": 4.5,
+    "location": "Washermenpet, Washermenpet",
+    "estimatedTimeArrival": "50-60 mins"
+  }
 
-const CardComponent = () => {
-    return <div className="card">
-        <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2026/6/19/76333369-ea7c-4dd8-990e-a3c22defc234_1374812.jpg"/>
-        <h2>{jsxHeading}</h2>
-        <p>Despite its fame as a resplendent island and a leading tourist destination, the secret of Sri Lanka’s attraction lies with its people. The spice addicted, cricket crazy and tea drinking people of Sri Lanka are famed for big smiles and a bigger heart. With a culture enriched with three thousand years of knowledge, Buddhism and many a colonial traditions, the Sri Lankan society is a potpourri of religions and races different yet similar in many a ways.</p>
-    </div>
+]
+
+const Header = () => {
+    return(
+        <div className="header">
+            <div className="app-logo-container">
+                <img alt="app-logo" src="https://img.logo.dev/swiggy.com?token=live_6a1a28fd-6420-4492-aeb0-b297461d9de2&size=512&retina=true&format=png"/>
+            </div>
+            <nav className="nav-bar">
+                <ul>
+                    <li>Offers</li>
+                    <li>Help</li>
+                    <li>Sign In</li>
+                    <li>Cart</li>
+                </ul>
+            </nav>           
+        </div>
+    );
+}
+
+const RestaurantContainer = ({reslist}) => {
+    return (
+        <div className="res-container">{
+            reslist.map(restaurant => {return <RestaurantCard key={restaurant.id} resData={restaurant}/>})}
+        </div>
+    );
+}
+
+const PopularFoodCarousal = (props) => {
+    console.log(props);
+    return(
+        <>
+          <h1>What's on your mind?</h1>
+          <div className="banner-first">  
+            {
+              props.imgCarsoul.map((m, idx) => {return (
+              <div key={idx} className="popular-foods">
+                <img src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/${m.imageId}`}/>
+              </div>) })
+            }
+          <hr/>
+          </div>
+        </>
+    );
+}
+
+
+
+const RestaurantCard = (restaurantData) => {
+    const {imageId, name, rating, cuisines, location, estimatedTimeArrival} = restaurantData?.resData;
+    return (
+        <div className="res-card">
+            <img src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${imageId}`}/>
+            <div className="res-card-content">
+                <h3>{name}</h3>
+                <h4>{rating+" "+estimatedTimeArrival}</h4>
+                <p>{cuisines.join(", ")}</p>
+                <p>{location.split(",")[1]}</p>
+            </div>
+        </div>
+    );
+}
+
+const Body = () => {
+    return(
+        <div className="body-container">
+            <PopularFoodCarousal imgCarsoul={foodCarousalImg}/>
+            <RestaurantContainer reslist={restaurantList}/>
+        </div>
+    );
+}
+
+const AppLayout = () => {
+    return(
+        <div>
+            <Header/>
+            <Body/>
+        </div>
+    );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<div className="cards"><CardComponent/><CardComponent/><CardComponent/><CardComponent/><CardComponent/><CardComponent/><CardComponent/></div>);
+root.render(<AppLayout/>);
