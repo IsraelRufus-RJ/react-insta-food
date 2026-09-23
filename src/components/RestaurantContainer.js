255fa66ae7e1ae17fa090fe2f4bbe81eecc93774
IsraelRufus-RJ <israelrufus2000@gmail.com>
2026-09-23 17:16:57 +0530

    Added a top-rated restaurant filter.

--- src/components/RestaurantContainer.js
+++ src/components/RestaurantContainer.js
@@ -1,9 +1,24 @@
+import { useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 
 const RestaurantContainer = ({reslist}) => {
+    const [restaurantList, setRestaurantList] = useState(reslist);
+
     return (
-        <div className="res-container">{
-            reslist.map(restaurant => {return <RestaurantCard key={restaurant.id} resData={restaurant}/>})}
+        <div>
+            <div className="res-user-config">
+                <button onClick={()=>{
+                    const filteredRestaurantList = restaurantList.filter((restaurant => restaurant.rating >= 4.5));
+                    setRestaurantList(filteredRestaurantList);
+                }}>
+                    Top Rated Restaurant
+                </button>
+            </div>
+            <div className="res-container">
+                {
+                    restaurantList.map(restaurant => {return <RestaurantCard key={restaurant.id} resData={restaurant}/>})
+                }
+            </div>
         </div>
     );
 }
